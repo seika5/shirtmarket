5a5fb8efbe2bf6808fb5309d476de7baa00d0867
Ryan Mar <Phyre205@gmail.com>
2023-04-28 10:01:00 -0700

    updates

--- shirtmarket/market/views.py
+++ shirtmarket/market/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'ricin',
+		'title': 'Market Post 1',
+		'content': 'Market Post 1 Content',
+		'date_posted': 'April 25, 2023'
+	},
+	{
+		'author': 'hurtburt',
+		'title': 'Market Post 2',
+		'content': 'Market Post 2 Content',
+		'date_posted': 'April 26, 2023'
+	}
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'home.html', context)
 
-def about(request):	
-	return HttpResponse('<h1>Blog About</h1>')
+def about(request):
+	return render(request, 'about.html')
