e003adfc44fefe3c8882720243602825e81f4add
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-07-18 10:42:03 -0700

    remove ratings and reviews

--- shirtmarket/market/admin.py
+++ shirtmarket/market/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Item, Favorite, Review, Order
+from .models import Item, Favorite, Order
 
 admin.site.register(Item)
 admin.site.register(Favorite)
-admin.site.register(Review)
 admin.site.register(Order)
