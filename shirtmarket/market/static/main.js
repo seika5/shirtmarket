c89d9e22110d050ad432ee33c7b787349fc981d5
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-08-04 23:58:20 -0700

    content animation and js

--- shirtmarket/market/static/main.js
+++ shirtmarket/market/static/main.js
@@ -6,13 +6,16 @@ fetch("/config/")
   const stripe = Stripe(data.publicKey);
 
   // Event handler
-  document.querySelector("#purchase").addEventListener("click", () => {
+  const purchase = document.querySelector("#purchase");
+  if(purchase){
+    purchase.addEventListener("click", () => {
     // Get Checkout Session ID
-    fetch("create-checkout-session/")
-    .then((result) => { return result.json(); })
-    .then((data) => {
-      // Redirect to Stripe Checkout
-      return stripe.redirectToCheckout({sessionId: data.sessionId})
-    })
-  });
+      fetch("create-checkout-session/")
+      .then((result) => { return result.json(); })
+      .then((data) => {
+        // Redirect to Stripe Checkout
+        return stripe.redirectToCheckout({sessionId: data.sessionId})
+      })
+    });
+  }
 });
